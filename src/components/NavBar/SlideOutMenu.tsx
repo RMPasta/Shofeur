e2a9cf49704de293c633621517b987104a52170a
RMPasta <rmpasta.code@gmail.com>
2023-07-05 19:12:00 -0400

    fix slide out width

--- src/components/NavBar/SlideOutMenu.tsx
+++ src/components/NavBar/SlideOutMenu.tsx
@@ -16,7 +16,7 @@ export default function SlideOutMenu(): JSX.Element {
       setShowMenu(!showMenu);
   };
 
-  const ulClassName = "w-2/5 md:w-1/5 h-screen flex flex-col justify-between bg-gray-100 text-gray-800 transition-left duration-700" + (!showMenu ? " w-1/6 absolute -left-80 top-20" : " w-2/5 md:w-1/3 lg:w-1/5 absolute left-0 top-20");
+  const ulClassName = "w-3/5 md:w-1/5 h-screen flex flex-col justify-between bg-gray-100 text-gray-800 transition-left duration-700" + (!showMenu ? " w-1/6 absolute -left-80 top-20" : " w-2/5 md:w-1/3 lg:w-1/5 absolute left-0 top-20");
 
   return (
     <div className='z-10'>
@@ -42,11 +42,11 @@ export default function SlideOutMenu(): JSX.Element {
             <li className='flex justify-center hover:bg-gray-200 w-full p-4 hover:cursor-pointer'>Add a listing</li>
         </ul>
         <ul className='w-full mb-20'>
-        <p className='flex justify-center font-bold text-xl py-4'>Built by</p>
+            <p className='flex justify-center font-bold text-xl py-4'>Built by</p>
             <li className='flex justify-center hover:bg-gray-200 w-full p-4 hover:cursor-pointer'><a href="https://rmpasta.github.io/RMPortfolio/" target="_blank">Ryan Malmos</a></li>
             <li className='flex justify-center hover:bg-gray-200 w-full p-4 hover:cursor-pointer'><a href="https://www.linkedin.com/in/ryan-malmos/" target="_blank">Linkedin</a></li>
             <li className='flex justify-center hover:bg-gray-200 w-full p-4 hover:cursor-pointer'><a href="https://github.com/RMPasta/Shofeur" target="_blank">Github Repo</a></li>
-            <p className='flex justify-center p-4'>rmpasta.code@gmail.com</p>
+            <p className='flex justify-center'>rmpasta.code@gmail.com</p>
         </ul>
       </div>
     </div>
